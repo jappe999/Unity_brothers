4a5cc732338d5ab5f3a7626e4fd38f3abbb439cb
Jasper <jappe_1999@outlook.com>
2015-03-09 17:51:09 +0100

    Arcade fix
    You can now, while you're in the air, move on the x-axis

--- Jump_until_the_end/Assets/Scripts/PlayerController.cs
+++ Jump_until_the_end/Assets/Scripts/PlayerController.cs
@@ -15,13 +15,14 @@ public class PlayerController : MonoBehaviour
 		if (controller.isGrounded)
 		{
 			//Geeft aan dat het personage alleen horizontaal kan lopen.
-			moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, 0.0f);
+			moveDirection = new Vector3 (Input.GetAxis("Horizontal"), 0.0f, 0.0f);
 			moveDirection = transform.TransformDirection(moveDirection);
 			moveDirection *= speed;
 			//Als de jump-toets wordt ingedrukt, springt het personage omhoog.
 			if(Convert.ToBoolean(Input.GetAxis("Jump")))
 			   moveDirection.y = jumpSpeed;
 		}
+		moveDirection.x = Input.GetAxis ("Horizontal") * speed;
 		//Door de zwaartekracht wordt het personage weer naar beneden gehaald.
 		moveDirection.y -= gravity * Time.deltaTime;
 		controller.Move(moveDirection * Time.deltaTime);

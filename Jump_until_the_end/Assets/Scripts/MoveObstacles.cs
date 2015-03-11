b798376d4ef1d458eaf960666408653b390076f6
Jasper <jappe_1999@outlook.com>
2015-03-11 18:19:40 +0100

    gewoon

---
+++ Jump_until_the_end/Assets/Scripts/MoveObstacles.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveObstacles : MonoBehaviour {
+	
+	void onTriggerEnter (Collider other) {
+		Destroy (other.gameObject);
+		Destroy (gameObject);
+	}
+}

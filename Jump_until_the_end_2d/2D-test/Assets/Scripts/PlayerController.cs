a745743af32846bbafea4425da5a5d77db05c2e8
Jasper <jappe_1999@outlook.com>
2015-03-15 19:36:01 +0100

    Instabiele versie 2d
    Graag nog niet bewerken...

---
+++ Jump_until_the_end_2d/2D-test/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System;
+
+public class PlayerController : MonoBehaviour
+{
+	public float speed;
+	public float jumpSpeed = .25f;
+	public float gravity = 2.0f;
+	private Vector3 moveDirection = Vector3.zero;
+	
+	void Update ()
+	{
+		CharacterController controller = GetComponent<CharacterController> ();
+		if (controller.isGrounded)
+		{
+			//Geeft aan dat het personage alleen horizontaal kan lopen.
+			moveDirection = new Vector3 (Input.GetAxis ("Horizontal"), 0.0f, 0.0f);
+			moveDirection = transform.TransformDirection(moveDirection);
+			moveDirection *= speed;
+			//Als de jump-toets wordt ingedrukt, springt het personage omhoog.
+			if(Convert.ToBoolean(Input.GetAxis ("Jump")))
+				moveDirection.y = jumpSpeed;
+		}
+		moveDirection.x = Input.GetAxis ("Horizontal") * speed;
+		//Door de zwaartekracht wordt het personage weer naar beneden gehaald.
+		moveDirection.y -= gravity * Time.deltaTime;
+		controller.Move(moveDirection * Time.deltaTime);
+	}
+}

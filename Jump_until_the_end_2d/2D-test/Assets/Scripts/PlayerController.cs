ef42cb1163cd5f9b3798822ada18eae9940be08a
Erik Baalbergen <erikbaalbergen@gmail.com>
2015-03-15 20:53:14 +0100

    Nieuwe versie van playerScript

--- Jump_until_the_end_2d/2D-test/Assets/Scripts/PlayerController.cs
+++ Jump_until_the_end_2d/2D-test/Assets/Scripts/PlayerController.cs
@@ -1,29 +1,49 @@
-﻿using UnityEngine;
+﻿﻿using UnityEngine;
 using System;
+using System.Math;
 
 public class PlayerController : MonoBehaviour
 {
-	public float speed;
-	public float jumpSpeed = .25f;
-	public float gravity = 2.0f;
-	private Vector3 moveDirection = Vector3.zero;
-	
-	void Update ()
-	{
-		CharacterController controller = GetComponent<CharacterController> ();
-		if (controller.isGrounded)
-		{
-			//Geeft aan dat het personage alleen horizontaal kan lopen.
-			moveDirection = new Vector3 (Input.GetAxis ("Horizontal"), 0.0f, 0.0f);
-			moveDirection = transform.TransformDirection(moveDirection);
-			moveDirection *= speed;
-			//Als de jump-toets wordt ingedrukt, springt het personage omhoog.
-			if(Convert.ToBoolean(Input.GetAxis ("Jump")))
-				moveDirection.y = jumpSpeed;
-		}
-		moveDirection.x = Input.GetAxis ("Horizontal") * speed;
-		//Door de zwaartekracht wordt het personage weer naar beneden gehaald.
-		moveDirection.y -= gravity * Time.deltaTime;
-		controller.Move(moveDirection * Time.deltaTime);
-	}
+    public float speed, jumpSpeed = .25f, gravity = 2.0f, mass;
+    private float heigthEnergy, verticalKineticEnergy;
+    private Vector3 moveDirection = Vector3.zero;
+    private CharacterController controller;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        animation.Play("Run");
+    }
+
+    void Update()
+    {
+        if (controller.isGrounded)
+        {
+            //Tells the character that it can only walk on the vertical axis
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, 0.0f);
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection *= speed;
+            //If the jump-button is pressed, the character will jump
+            if (Convert.ToBoolean(Input.GetAxis("Jump")))
+            {
+                moveDirection.y = jumpSpeed;
+
+            }
+        }
+        moveDirection.x = Input.GetAxis("Horizontal") * speed;
+        //Door de zwaartekracht wordt het personage weer naar beneden gehaald.
+        moveDirection.y -= gravity * Time.deltaTime;
+        controller.Move(moveDirection * Time.deltaTime);
+    }
+
+    private float calculateNewPositionOnYAsix()
+    {
+        if (controller.isGrounded)
+            verticalKineticEnergy = 0;
+
+        float speed = Math.Sqrt(2 * verticalKineticEnergy / mass);
+        float deltaDistance = speed * time.deltaTime;
+
+        return deltaDistance;
+    }
 }
